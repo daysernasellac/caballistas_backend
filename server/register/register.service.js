214d684dcba43815dbda3a012dd87be2feb49498
karen <karenchiquito.s@gmail.com>
2020-05-25 03:21:05 -0500

    Refactorización de codigo

--- server/register/register.service.js
+++ server/register/register.service.js
@@ -1,27 +1,12 @@
 "use strict";;
-const express = require('express');
 const database = require('../database');
-const ejemplo = require('../login/login.router')
-
-const httpMessages = {
-    onValidationError: {
-        success: false,
-        message: 'Please enter email and password',
-    },
-    onSaveError: {
-        success: false,
-        message: 'The email address already exist',
-    },
-    onUserSaveSuccess: {
-        success: true,
-        message: 'Successfully created new user',
-    }
-};
+const Cliente = require('../models/Cliente')
+const _ = require('lodash');
 
 function findUserByDocument({ tipo_documento, numero_documento }) {
     let query = 'SELECT * FROM INFORMACION_CLIENTE WHERE TIPO_DOCUMENTO = $1 AND NUMERO_DOCUMENTO = $2';
     return database.query(query, [tipo_documento, numero_documento])
-        .then(response => response.rows)
+        .then(response => response.rows[0])
         .catch(err => { throw new Error(err) });
 }
 
@@ -34,11 +19,30 @@ function findUserByEmail({ correo }) {
 }
 
 // Register new user 
-function registerUser2(params) {
-    let text = 'INSERT INTO INFORMACION_CLIENTE(ID_CLIENTE, TIPO_DOCUMENTO, NUMERO_DOCUMENTO, NOMBRES, TELEFONO, DIRECCION, ID_DEPARTAMENTO, ID_MUNICIPIO, APELLIDOS) VALUES (nextval($1), $2, $3, $4, $5, $6, $7, $8, $9)';
-    return database.query(text, params)
-        .then(response => response.rows)
-        .catch(err => { throw new Error(err) });
+async function registerInformacionCliente(params) {
+  let text = 'INSERT INTO INFORMACION_CLIENTE(ID_CLIENTE, TIPO_DOCUMENTO, NUMERO_DOCUMENTO, NOMBRES, TELEFONO, DIRECCION, ID_DEPARTAMENTO, ID_MUNICIPIO, APELLIDOS) VALUES (nextval($1), $2, $3, $4, $5, $6, $7, $8, $9)';
+  let cliente = new Cliente(params);
+  let arrayCliente = cliente.toArray();
+
+  arrayCliente[0] = 'informacion_cliente_id_cliente_seq';
+
+  return database.query(text, arrayCliente)
+    .then(async (response) => {
+      let res = await findUserByDocument({
+        tipo_documento: params.tipo_documento,
+        numero_documento: params.numero_documento
+      });
+
+      return _.merge(response, res);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+async function registerUser2(params) {
+  let informacionCliente = await registerInformacionCliente(params);
+  console.log(informacionCliente)
 }
 
 module.exports = {
