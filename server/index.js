b6e63c2721f8bce852829e13512cd27e450ee15b
karen <karenchiquito.s@gmail.com>
2020-10-28 22:23:11 -0500

    Se quita coreOptions

--- server/index.js
+++ server/index.js
@@ -8,17 +8,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('../swagger');
 const cors = require('cors')
 
-var whitelist = ['http://localhost:3000/'];
-var corsOptions = {
-    origin: function(origin, callback){
-        if(whitelist.indexOf(origin) != -1){
-            callback(null, true)
-        }else{
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
-
 module.exports = function() {
     let server = express(),
         create,
@@ -37,7 +26,7 @@ module.exports = function() {
         server.use(cookieParser());
         server.use(logger('dev'));
         server.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-        server.use(cors(corsOptions));
+        server.use(cors());
 
         // set up routes
         routes.init(server);
