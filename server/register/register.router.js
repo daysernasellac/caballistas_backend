0039ec1c8fb28ee0c08c13489eda6c63175ff8dc
karen <karenchiquito.s@gmail.com>
2020-12-04 20:44:35 -0500

    se cambia get por post

--- server/register/register.router.js
+++ server/register/register.router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const registerController = require('./register.controller');
 let router = express.Router();
 
-router.get('/', registerController.registerUser);
+router.post('/', registerController.registerUser);
 router.get('/informacionCliente/:tipo_documento&:numero_documento', registerController.getInformacionClienteByDocumento);
 router.get('/informacionCliente/correo/:correo', registerController.getInformacionClienteByEmail);
 router.post('/finalizarRegistro', registerController.finalizarRegistro);
